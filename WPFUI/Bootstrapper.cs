6c7904f32339df7687c6fa21bbb6073ff9aa3a21
Tarmo <liivamagi.tarmo@gmail.com>
2024-04-03 22:54:01 +0300

    first commit, caliburn setup and table layout

---
+++ WPFUI/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFUI.ViewModels;
+
+
+namespace WPFUI
+{
+    public class Bootstrapper:BootstrapperBase
+    {
+        public Bootstrapper() 
+        {
+            Initialize();
+            LogManager.GetLog = type => new DebugLog(type);
+        }
+
+        protected override async void OnStartup(object sender, StartupEventArgs e)
+        {
+            await DisplayRootViewForAsync(typeof(ShellViewModel));
+        }
+
+
+    }
+}
---
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+        private List<int> _number = Enumerable.Range(1, 36).ToList();
+        
+        public List<int> Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                _number = value;
+                NotifyOfPropertyChange(() => Number);
+                
+            }
+        }
+
+        public ShellViewModel()
+        {
+
+        }
+        
+      
+    }
+}
